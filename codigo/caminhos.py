c66ae2ff82d6285e2391c89383503b2f94642aeb
JoaoVitorBaggio <72949356+JoaoVitorBaggio@users.noreply.github.com>
2024-08-24 18:47:22 -0300

    tirei rating pq é mt grande

--- codigo/caminhos.py
+++ codigo/caminhos.py
@@ -5,7 +5,4 @@ pasta_codigo = Path(__file__).resolve().parent
 
 pasta_raiz = pasta_codigo.parent
 
-pasta_entrada = pasta_raiz / "entrada"
-
-pasta_saida = pasta_raiz / "saida"
-
+pasta_dados = pasta_raiz / "dados"
